29426411dd3ee5f99843f51d1c27cbeeb3a5f738
evgvac <joniy@yandex.ru>
2023-05-01 10:37:43 +0300

    1

--- api.py
+++ api.py
@@ -2,13 +2,13 @@
 #import certifi
 import requests
 #from urllib.request import urlopen
-requests.get('http://srv-pen-solar.bft.local', verify=False)
+#requests.get('http://srv-pen-solar.bft.local', verify=False)
 #data = {'srv-pen-solar.bft.local':'app/api/v1/user/token/endless'}
 #url = 'http://srv-pen-solar.bft.local/app/api/v1/user/token/endless'
 #r = requests.post(url, data=data, verify='/home/evgvac/Документы/БФТ/_.bft.local.pem')
 
 
-response = requests.get("http://srv-pen-solar.bft.local/app/api/v1/user/token/endless")
+response = requests.get("https://srv-pen-solar.bft.local/app/api/v1/health")
 #urlopen(request, context=ssl.create_default_context(cafile=certifi.where()))
 print (response.json())
 
